403ceedb719389f0971c1ae810eb4ce2ba8e28b3
OlenaP <nonna467@gmail.com>
2020-03-24 21:28:09 +0200

    created header and main

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,44 +1,38 @@
-import React from 'react'
+import React from "react";
 
-const Footer =()=>{
-	return (
-		<footer className="footer">
-		<div className="container">
-			<div className="row">
-				<div className="col-md-2">
-					ghbdrgb
-				</div>
-			<div className="row">
-				<div className="col-md-6">
-					<nav>
-						<ul className="menu2 row">
-					<li><a href="">Главная</a></li>
-					<li><a href="">Наши работы</a></li>
-					<li><a href="">Отзывы</a></li>
-					<li><a href="">Цены и услуги</a></li>
-					<li><a href="">Новости</a></li>
-					<li><a href="">Контакты</a></li>
-						</ul>
-					</nav>
-				</div>
-				<div className="row">
-				<div className="col-md-2">
-					<div className="contacts">
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-2">ghbdrgb</div>
+          <div className="row">
+            <div className="col-md-6">
+              <nav>
+                <ul className="menu2 row">
+                  <li><a href="url">Главная</a></li>
+                  <li><a href="url">Наши работы</a></li>
+                  <li><a href="url">Отзывы</a></li>
+                  <li><a href="url">Цены и услуги</a></li>
+                  <li><a href="url">Новости</a></li>
+                  <li><a href="url">Контакты</a></li>
+                </ul>
+              </nav>
+            </div>
+            <div className="row">
+              <div className="col-md-2">
+                <div className="contacts">
+                  <div>+38(093) 788-98-98</div>
+                  <div>smm-lab-group</div>
+                  <div>info@smm-lab.com</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-					<div>+38(093) 788-98-98</div>
-
-					<div>smm-lab-group</div>
-
-					<div>info@smm-lab.com</div>
-				</div>
-				</div>
-		</div>
-	</div>
-    </div>
-    </div>
-	</footer> 
-  
-	)
-}
-
-export default Footer
+export default Footer;
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -5,58 +5,62 @@ import logo from './Logo/logo.jpg'
 
 const Header =()=>{
 	return (
-<header className="header">
-		<div className="container">
-			<div className="row">
-				<div className="col-md-8">
-					<nav>
-						<ul className="menu row">
-					<li className="menu_list"><a href="">Главная</a>
-					<li><a href="">Продвижение в ВКонтакте</a>
-					<ul className="submenu">
-						<li><a href="">ВКонтакте Standart</a></li>
-						<li><a href="">ВКонтакте Business</a></li>
-						<li><a href="">ВКонтакте Pro+</a></li>
-					</ul>
-					</li>
-					<li><a href="">Продвижение в Facebook</a>
-					<ul className="submenu">
-						<li><a href="">Facebook Standart</a></li>
-						<li><a href="">Facebook Business</a></li>
-						<li><a href="">Facebook Pro+</a></li>
-					</ul>
-					</li>
-					<li><a href="">Продвижение в Одноклассники</a>
-					<ul className="submenu">
-						<li><a href="">Одноклассники Standart</a></li>
-						<li><a href="">Одноклассники Business</a></li>
-						<li><a href="">Одноклассники Pro+</a></li>
-					</ul>
-					</li>
-					<li><a href="">Продвижение в Instagram</a>
-					<ul className="submenu">
-						<li><a href="">Instagram Standart</a></li>
-						<li><a href="">Instagram Business</a></li>
-						<li><a href="">Instagram Pro+</a></li>
-					</ul>
-					</li>
-                    </li>	
-                    </ul>
-					</nav>
-					</div>
-			<div className="row">
-			<div className="col-md-2">
-							<div className="logo">
-						<a href=""><img src={logo}></img></a></div>
-				
-					</div>
-				</div>
-			</div>
-			</div>
-	
-		
-	</header>
-    )
+    <header className="header">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8">
+            <nav>
+              <ul className="menu row">
+                <li className="menu_list"><a href="url">Главная</a>{" "}</li>
+                <li><a href="url">Продвижение в ВКонтакте</a>
+                <ul className="submenu">
+                  <li><a href="url">ВКонтакте Standart</a></li>
+                  <li><a href="url">ВКонтакте Business</a></li>
+                  <li><a href="url">ВКонтакте Pro+</a></li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="url">Продвижение в Facebook</a>
+                  <ul className="submenu">
+                    <li><a href="url">Facebook Standart</a></li>
+                    <li><a href="url">Facebook Business</a></li>
+                    <li><a href="url">Facebook Pro+</a></li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="url">Продвижение в Одноклассники</a>
+                  <ul className="submenu">
+                    <li><a href="url">Одноклассники Standart</a></li>
+                    <li><a href="url">Одноклассники Business</a></li>
+                    <li><a href="url">Одноклассники Pro+</a>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="url">Продвижение в Instagram</a>
+                  <ul className="submenu">
+                    <li><a href="url">Instagram Standart</a></li>
+                    <li><a href="url">Instagram Business</a></li>
+                    <li><a href="url">Instagram Pro+</a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="row">
+            <div className="col-md-2">
+              <div className="logo">
+                <a href="url">
+                  <img src={logo} alt="icon"/>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
     }
 
 export default Header
